e497165982012b04c3ed08cd99bc1d22e7da1042
justc0der <yrizhniv@evinced.com>
2021-09-26 23:11:21 +0300

    feat: use es6 import/export

--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,14 +1,12 @@
-namespace App {
-	// autobind decorator
-	export function autobind(_: any, _2: string, description: PropertyDescriptor) {
-		const originalMethod = description.value;
-		const adjustDescriptor: PropertyDescriptor = {
-			configurable: true,
-			get() {
-				const boundFn = originalMethod.bind(this);
-				return boundFn;
-			}
-		};
-		return adjustDescriptor;
-	}
+// autobind decorator
+export function autobind(_: any, _2: string, description: PropertyDescriptor) {
+	const originalMethod = description.value;
+	const adjustDescriptor: PropertyDescriptor = {
+		configurable: true,
+		get() {
+			const boundFn = originalMethod.bind(this);
+			return boundFn;
+		}
+	};
+	return adjustDescriptor;
 }
--- src/state/project-state.ts
+++ src/state/project-state.ts
@@ -1,56 +1,57 @@
-namespace App {
-	type Listener<T> = (items: T[]) => void;
+import { Project } from '../model/project-model.js';
+import { ProjectStatus } from '../model/project-model.js';
 
-	 class State<T> {
-		protected listeners: Listener<T>[] = [];
+type Listener<T> = (items: T[]) => void;
 
-		addListener(func: Listener<T>) {
-			this.listeners.push(func);
-		}
+class State<T> {
+	protected listeners: Listener<T>[] = [];
+
+	addListener(func: Listener<T>) {
+		this.listeners.push(func);
+	}
+}
+
+export class ProjectState extends State<Project> {
+	private projects: Project[] = [];
+	private static instance: ProjectState;
+
+	private constructor() {
+		super();
 	}
 
-	export class ProjectState extends State<Project> {
-		private projects: Project[] = [];
-		private static instance: ProjectState;
-
-		private constructor() {
-			super();
-		}
-
-		static getInstance() {
-			if (this.instance) {
-				return this.instance;
-			}
-			this.instance = new ProjectState();
+	static getInstance() {
+		if (this.instance) {
 			return this.instance;
 		}
+		this.instance = new ProjectState();
+		return this.instance;
+	}
 
-		addProject(title: string, description: string, numOfPeople: number) {
-			const newProj = new Project(
-				Math.random().toString(),
-				title,
-				description,
-				numOfPeople,
-				ProjectStatus.Active
-			);
-			this.projects.push(newProj);
+	addProject(title: string, description: string, numOfPeople: number) {
+		const newProj = new Project(
+			Math.random().toString(),
+			title,
+			description,
+			numOfPeople,
+			ProjectStatus.Active
+		);
+		this.projects.push(newProj);
+		this.updateListeners();
+	}
+
+	moveProject(projectId: string, newStatus: ProjectStatus) {
+		const project = this.projects.find((prj) => prj.id === projectId);
+		if (project && project.status !== newStatus) {
+			project.status = newStatus;
 			this.updateListeners();
-		}
-
-		moveProject(projectId: string, newStatus: ProjectStatus) {
-			const project = this.projects.find((prj) => prj.id === projectId);
-			if (project && project.status !== newStatus) {
-				project.status = newStatus;
-				this.updateListeners();
-			}
-		}
-
-		private updateListeners() {
-			for (const listenerFn of this.listeners) {
-				listenerFn(this.projects.slice());
-			}
 		}
 	}
 
-	export const projectState = ProjectState.getInstance();
+	private updateListeners() {
+		for (const listenerFn of this.listeners) {
+			listenerFn(this.projects.slice());
+		}
+	}
 }
+
+export const projectState = ProjectState.getInstance();
